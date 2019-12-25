128da06223560ae093086bd621be0155b4b4df1b
nnao45 <n4sekai5y@gmail.com>
2019-12-25 10:01:00 +0900

    1st

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "cci-scala"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
---
+++ src/main/scala/one_one/Main.scala
@@ -0,0 +1,15 @@
+package one_one
+
+// 文字列内に含まれる文字がユニークか探す
+
+object Main extends App {
+  val input = io.StdIn.readLine
+  def judge: Boolean = {
+    input.foreach(e => {
+      if (input.filter(a => a == e).length > 1) return false
+    })
+    true
+  }
+
+  println(if (judge) "YES" else "NO")
+}
